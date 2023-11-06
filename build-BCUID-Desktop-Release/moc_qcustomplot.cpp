f8f046fd26d0d5d486f25556729c7390ba9c6cb6
Instruments04 <109733238+Instruments04@users.noreply.github.com>
2023-11-06 14:38:03 +0530

    V0.01

--- build-BCUID-Desktop-Release/moc_qcustomplot.cpp
+++ build-BCUID-Desktop-Release/moc_qcustomplot.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../BCUID/qcustomplot.h"
+#include "../../Biochemistry_Updates/BCUID/qcustomplot.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #include <QtCore/QSharedPointer>
